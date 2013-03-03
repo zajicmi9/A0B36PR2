d3688b85e2d16e24a17320bb199ecfecba0c01ea
Milan <Milan@147.32.90.132>
2013-03-03 10:36:29 +0100

    Hello

--- Semestralka/src/semestralka/Semestralka.java
+++ Semestralka/src/semestralka/Semestralka.java
@@ -14,6 +14,6 @@ public class Semestralka {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello");
     }
 }
