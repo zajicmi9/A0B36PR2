6ca8dbdc33eb00d7c858bfe86c943dc2b0c0cb19
Milan <Milan@PC-Milan>
2013-04-21 22:12:13 +0200

    update, operation in welcome window

---
+++ Semestralka/src/semestralka/OperationStart.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semestralka;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Milan
+ */
+public class OperationStart implements ActionListener {
+    
+    
+   @Override
+   public void actionPerformed(ActionEvent o) {
+        Button b = (Button) o.getSource();
+        WWindow w  = (WWindow) b.getParent().getParent().getParent().getParent();   
+        RulletWindow r = new RulletWindow ();  
+        int players,credit;
+        players = (int) Double.parseDouble(w.list.getSelectedItem());
+        credit = (int) Double.parseDouble(w.pole.getText());
+        
+        switch (b.getText()) {
+            case"NEW GAME":
+                        if(credit>0){
+                             w.setVisible(false);
+                             r.setVisible(true);
+                        } else { 
+                        }
+                        System.out.println(players);
+                        System.out.println(credit);
+                break;
+            case"LOAD GAME":
+                
+                break;
+        }
+        
+        
+
+            
+        
+        
+            }
+}
